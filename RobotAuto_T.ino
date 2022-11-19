61a2dfd76297e9f2516922d80722080c8143f6a5
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2022-11-19 13:55:50 +0100

    add

---
+++ RobotAuto_T.ino
@@ -0,0 +1,103 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-one-to-many-esp32-esp8266/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include <driver/adc.h>
+
+
+// REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress1[] = {0x48, 0x3F, 0xDA, 0xA4, 0x36, 0x57};
+uint8_t broadcastAddress2[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t broadcastAddress3[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+typedef struct test_struct {
+  uint16_t x;
+  int y;
+} test_struct;
+
+test_struct test;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  //Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  //Serial.print(macStr);
+  //Serial.print(" send status:\t");
+  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  WiFi.mode(WIFI_STA);
+ 
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  esp_now_register_send_cb(OnDataSent);
+   
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // register second peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress2, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  /// register third peer
+  memcpy(peerInfo.peer_addr, broadcastAddress3, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+   pinMode(5,INPUT);
+   pinMode(8,INPUT);
+   adc1_config_width(ADC_WIDTH_BIT_12);
+   adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_0);
+   adc1_config_channel_atten(ADC1_CHANNEL_1,ADC_ATTEN_DB_0);
+  
+}
+ 
+void loop() {
+  test.x = adc1_get_raw(ADC1_CHANNEL_0);
+  test.y = adc1_get_raw(ADC1_CHANNEL_3);
+ Serial.print(test.x);
+  Serial.print(" ");
+  Serial.println(test.y);
+  esp_err_t result = esp_now_send(0, (uint8_t *) &test, sizeof(test_struct));
+//Serial.print("result: ");
+   //Serial.println(result);
+  if (result == ESP_OK) {
+    //Serial.println("Sent with success");
+    //Serial.print("test.x: ");
+   
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(20);
+}
